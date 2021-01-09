2e5dcfb0a92bf7e7f72c0550203ad788d9e50fb7
Franco Tegaldi <francoteg@gmail.com>
2021-01-08 22:06:11 -0300

    basic api endpoint and config.

---
+++ helpers/index.js
@@ -0,0 +1,12 @@
+export const processTopPostsData = (responseData) => {
+  return responseData.data.children.map((post) => {
+    return {
+      title: post.data.title,
+      author: post.data.author,
+      created_at: post.data.created_utc,
+      thumbnail: post.data.thumbnail,
+      url: post.data.url,
+      comments: post.data.num_comments,
+    };
+  });
+};
---
+++ pages/api/top.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const axios = require('axios');
+const { processTopPostsData } = require('@helpers');
+
+const getTopPosts = async () => {
+  try {
+    let res = await axios.get('https://reddit.com/top.json?limit=50');
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default async (req, res) => {
+  try {
+    let posts = await getTopPosts();
+    let processedPosts = processTopPostsData(posts);
+    res.status(200).json({ processedPosts });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
